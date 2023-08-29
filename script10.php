fb07feb073c53f7bbc9007f91681bd741d3b079f
Hemangi-Chavda <143484439+Hemangi-Chavda@users.noreply.github.com>
2023-08-29 13:34:10 +0530

    php assignment

---
+++ script10.php
@@ -0,0 +1,5 @@
+<?php
+$name ;
+$age;
+echo "Hello,john!you are 25 years old";
+?>
---
+++ script9.php
@@ -0,0 +1,6 @@
+<?php  
+$str = 'Javatpoint';  
+echo "Your String is:".$str;  
+echo "<br>";  
+echo "By using strlen() function:".strlen($str); 
+?>  
